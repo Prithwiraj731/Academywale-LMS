e4220983abed4fa0228a3144ff40fefb21edca4b
Prithwiraj731 <prithwi1016@gmail.com>
2025-07-27 15:31:07 +0530

    UI Modified !

--- client/src/components/layout/Navbar.jsx
+++ client/src/components/layout/Navbar.jsx
@@ -41,7 +41,7 @@ export default function Navbar() {
                 <path d="M2.003 5.884L10 9.882l7.997-3.998A2 2 0 0016 4H4a2 2 0 00-1.997 1.884z" />
                 <path d="M18 8.118l-8 4-8-4V14a2 2 0 002 2h12a2 2 0 002-2V8.118z" />
               </svg>
-              <span>support@facultywala.com</span>
+              <span>support@academywale.com</span>
             </div>
           </div>
           <div className="flex items-center space-x-4">
