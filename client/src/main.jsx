ad16ea5f850e9e5315fa31f05742f280e772cbfd
Prithwiraj731 <prithwi1016@gmail.com>
2025-09-03 02:35:09 +0530

    Responsiveness Modified !

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,8 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
-import './data/debugFacultyUpdates.js';
-import './data/applyFacultyUpdates.js';
 import { AuthProvider } from './context/AuthContext';
 import { BrowserRouter } from 'react-router-dom';
 
