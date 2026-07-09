6388e7698cd4017f986d1d40db5f8fe32d447e0b
Prithwiraj731 <prithwi1016@gmail.com>
2026-07-09 16:16:52 +0530

    fix: wrap admin client router tree in AuthProvider to prevent useAuth context crash

--- admin/client/src/main.jsx
+++ admin/client/src/main.jsx
@@ -3,16 +3,19 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Admin from '../../../client/src/pages/Admin'
 import AdminDashboard from '../../../client/src/pages/AdminDashboard'
+import { AuthProvider } from '../../../client/src/context/AuthContext'
 import './index.css'
 
 createRoot(document.getElementById('root')).render(
     <React.StrictMode>
-        <BrowserRouter>
-            <Routes>
-                <Route path="/admin" element={<Admin />} />
-                <Route path="/admin-dashboard" element={<AdminDashboard />} />
-                <Route path="*" element={<Admin />} />
-            </Routes>
-        </BrowserRouter>
+        <AuthProvider>
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/admin" element={<Admin />} />
+                    <Route path="/admin-dashboard" element={<AdminDashboard />} />
+                    <Route path="*" element={<Admin />} />
+                </Routes>
+            </BrowserRouter>
+        </AuthProvider>
     </React.StrictMode>
 )
